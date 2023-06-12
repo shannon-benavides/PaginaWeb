b7f4c4f08e4361923ab92584c7bb6907fb5b3baf
shannon-benavides <shannon.benavides.viera@gmail.com>
2023-06-12 19:01:29 -0400

    cambios

---
+++ script.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+    configurarInicio ();
+    $('.myicono' ).click(function(e){
+        $('#navbar ul').toggleClass('lista-sm' );
+    })
+    function configurarInicio () {
+        var urlPath = window.location.pathname;
+        console.log(urlPath);
+        $('nav a').each(function(){
+            var href = $(this).attr('href')
+            console.log(href);
+            var indice = urlPath.length - href.length;
+            console.log(urlPath.substring(indice));
+            if(urlPath.substring(indice)===href) {
+                console.log('match');
+                $(this).closest('li').addClass('active');
+            }
+        })
+    }
+})
